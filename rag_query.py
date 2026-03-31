0fcf7628cb32138ec30ed9b970953af8574a60d0
vardhan-vvs <velivelavardhan@gmail.com>
2026-03-31 16:26:59 -0400

    rag without UI

--- rag_query.py
+++ rag_query.py
@@ -7,3 +7,33 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 from langchain_ollama import OllamaLLM
 
+index = faiss.read_index("fv_index.faiss")
+texts = pickle.load(open("chunks.pkl", "rb"))
+
+model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda')
+llm = OllamaLLM(model = "llama3:instruct")
+
+def pr_rag(query):
+    query_embedding = model.encode([query], convert_to_numpy=True)
+    data, indices = index.search(query_embedding, k=5)
+
+    relevant_chunks = [texts[i] for i in indices[0]]
+    context = "\n".join(relevant_chunks)
+    prompt = f"""
+    You are a research paper assistant. You will answer the question only using the context below.
+    If you don't know the answer, say you don't know. Do not make up an answer.
+    If the answer is not in the context, say "The document does not contain this info."
+    context:
+    {context}
+    question:
+    {query}
+    Answer:
+    """
+
+    answer = llm.invoke(prompt)
+    return answer
+
+if __name__ == "__main__":
+    query = "What is this paper about?"
+    answer = pr_rag(query)
+    print("Answer:", answer)
